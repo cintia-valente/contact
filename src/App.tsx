22122355aedc0ffaf15a82911ac24cc6d57131a5
cintia-valente <cintiavalente@live.com>
2023-12-22 11:38:23 -0300

    :recycle: refactor: mudança de pasta do navbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { Footer } from './components/Footer';
 import Home from './pages/Home';
 import Contact from './pages/Contact';
 import User from './pages/User';
-import Navbar from './pages/Navbar';
+import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
